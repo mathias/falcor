011b89dbdf7805e23a790d148eef9f1577604901
Pedro Andrade <pedro.rjandrade@gmail.com>
2014-01-24 14:50:51 -0300

    Move the factories to spec/support/ folder

--- spec/factory_spec.rb
+++ spec/factory_spec.rb
@@ -53,18 +53,4 @@ describe 'Factory' do
       })
     end
   end
-
-  def factories
-    Falcor::Fabricator.define :author do
-      field :username, "matt@example.com"
-      field :password, "password"
-    end
-
-    Falcor::Fabricator.define :blog_post do
-      field :title, "My post"
-      field :body, "Lots of text"
-      association :author
-    end
-  end
-
 end
